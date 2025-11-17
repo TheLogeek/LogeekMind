03dc9dc5f03d566296875b2c2a526c7d4f8e719a
Solomon Adenuga <solomonadenuga8@outlook.com>
2025-11-17 00:21:19 -0800

    Update LogeekMind.py

--- LogeekMind.py
+++ LogeekMind.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="LogeekMind: Your AI Academic Assistant",
-    page_icon="📘",
+    page_icon="favicon.ico",
     layout="wide",
     initial_sidebar_state="expanded"
 )
