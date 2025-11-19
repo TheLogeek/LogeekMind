3c183f27d3438fda9a6742c336559af4ab04a02f
TheLogeek <solomonadenuga8@outlook.com>
2025-11-19 07:49:44 -0800

    updated ai teacher

--- pages/AI_Teacher.py
+++ pages/AI_Teacher.py
@@ -28,66 +28,68 @@ if prompt := st.chat_input("Ask your teacher a question..."):
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    try:
-        contents = []
+    with st.spinner("📝Preparing Notes..."):
 
-        for msg in st.session_state.messages:
-            part = types.Part.from_text(text=msg["text"])
+        try:
+            contents = []
 
-            content_obj = types.Content(
-                role=msg["role"],
-                parts=[part]
+            for msg in st.session_state.messages:
+                part = types.Part.from_text(text=msg["text"])
+
+                content_obj = types.Content(
+                    role=msg["role"],
+                    parts=[part]
+                )
+
+                contents.append(content_obj)
+
+            response = client.models.generate_content(
+                model=model_name,
+                config=types.GenerateContentConfig(
+                    system_instruction=System_instruction
+                ),
+                contents=contents
             )
 
-            contents.append(content_obj)
-
-        response = client.models.generate_content(
-            model=model_name,
-            config=types.GenerateContentConfig(
-                system_instruction=System_instruction
-            ),
-            contents=contents
-        )
-
-        with st.chat_message("assistant"):
-            st.markdown(response.text)
-        st.session_state.messages.append({"role": "model", "text": response.text})
-        st.download_button(
-            label="Download Notes",
-            data=response.text.encode('utf-8'),
-            file_name=f"{prompt} lecture_notes.txt",
-            mime="text/plain"
-        )
+            with st.chat_message("assistant"):
+                st.markdown(response.text)
+            st.session_state.messages.append({"role": "model", "text": response.text})
+            st.download_button(
+                label="Download Notes",
+                data=response.text.encode('utf-8'),
+                file_name=f"{prompt} lecture_notes.txt",
+                mime="text/plain"
+            )
 
 
 
-    except APIError as e:
+        except APIError as e:
 
-        error_text = str(e)
+            error_text = str(e)
 
-        if "429" in error_text or "RESOURCE_EXHAUSTED" in error_text.upper():
+            if "429" in error_text or "RESOURCE_EXHAUSTED" in error_text.upper():
 
-            if "api_key" in st.session_state:
-                del st.session_state.api_key
+                if "api_key" in st.session_state:
+                    del st.session_state.api_key
 
-            st.error("🚨 **Quota Exceeded!** Your Gemini API key has hit it's limit")
+                st.error("🚨 **Quota Exceeded!** Your Gemini API key has hit it's limit")
 
-            st.stop()
+                st.stop()
 
-        elif "503" in error_text:
+            elif "503" in error_text:
 
-            st.markdown("The Gemini AI model is currently experiencing high traffic. Please try again later. "
+                st.markdown("The Gemini AI model is currently experiencing high traffic. Please try again later. "
 
                         "Thank you for your patience!")
 
-            st.info(
-                "In the meantime, you can try other non-AI features **(GPA Calculator, Study Scheduler Lecture Note-to-Audio Converter, Lecture Audio-to-Text Converter)**")
+                st.info(
+                    "In the meantime, you can try other non-AI features **(GPA Calculator, Study Scheduler Lecture Note-to-Audio Converter, Lecture Audio-to-Text Converter)**")
 
-        else:
+            else:
 
-            st.error(f"An API error occurred during generation: {e}")
+                st.error(f"An API error occurred during generation: {e}")
 
 
-    except Exception as e:
+        except Exception as e:
 
-        st.error(f"An unexpected error occurred: {e}")
+            st.error(f"An unexpected error occurred: {e}")
