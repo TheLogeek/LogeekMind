fed9eac6d6de7fd2c2f9eafb381ec451269872a9
Solomon Adenuga <solomonadenuga8@outlook.com>
2025-12-04 22:18:24 +0100

    Update LogeekMind.py

--- LogeekMind.py
+++ LogeekMind.py
@@ -24,6 +24,13 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+ADMIN_ID = st.secrets["ADMIN_ID"]
+
+
+if "user" in st.session_state:
+    if st.session_state.user.id == ADMIN_ID:
+        st.sidebar.page_link("pages/admin_dashboard.py", label="Admin Dashboard")
+
 
 def render_auth_header():
     """Renders the login/logout status in the main app body."""
