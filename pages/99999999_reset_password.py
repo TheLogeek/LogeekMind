bac02f712a475de3a37678ab73cd6608180859cf
Solomon Adenuga <solomonadenuga8@outlook.com>
2025-12-11 01:53:37 -0800

    Update 99999999_reset_password.py

--- pages/99999999_reset_password.py
+++ pages/99999999_reset_password.py
@@ -4,7 +4,7 @@ from supabase import create_client
 @st.cache_resource
 def init_supabase():
     url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]  # anon/public key
+    key = st.secrets["SUPABASE_KEY"]
     return create_client(url, key)
 
 supabase = init_supabase()
